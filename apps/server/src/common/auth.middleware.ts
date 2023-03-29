907a6dd89071adee04c181402ebc3bb44d5a4f05
myWsq <wsq961@outlook.com>
2023-03-30 02:01:16 +0800

    fix: express type error

--- apps/server/src/common/auth.middleware.ts
+++ apps/server/src/common/auth.middleware.ts
@@ -1,5 +1,4 @@
 import { Inject, Injectable, NestMiddleware } from '@nestjs/common';
-import { Request, Response, NextFunction } from 'express';
 import { AuthService } from './auth.service';
 
 @Injectable()
@@ -7,7 +6,7 @@ export class AuthMiddleware implements NestMiddleware {
   @Inject()
   private _authService: AuthService;
 
-  use(req: Request, _res: Response, next: NextFunction) {
+  use(req: any, _res: any, next: any) {
     const adminToken = req.headers['x-admin-token'];
     const userToken = req.headers['authorization'];
     if (adminToken) {
